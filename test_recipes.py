8fc3c739832aff519cb13ff810ba8b2be41c466a
Севостьянова София <somisevostyanova@edu.hse.ru>
2026-06-02 21:26:32 +0300

    Реализованы тесты для класса Recipe

--- test_recipes.py
+++ test_recipes.py
@@ -1,4 +1,6 @@
+import pytest
 from classes import Ingredient
+from classes import Recipe
 
 def test_init_name_quantity_unit():
     ingredient = Ingredient('Bread',300.0,'г')
@@ -22,3 +24,35 @@ def test_eq1_name_quantity_unit():
     assert ingredient1 != ingredient4
     assert ingredient1 == ingredient5
 
+def test_init_title_ingredients():
+    ingredient = Ingredient('Bread',300.0,'г')
+    recipe = Recipe('Sandwich',[ingredient])
+    assert recipe.title == 'Sandwich'
+    assert len(recipe.ingredients) == 1
+def test_add_ingredient1():
+    ingredient = Ingredient('Bread',300.0,'г')
+    recipe = Recipe('Sandwich',[ingredient])
+    ingredient2 = Ingredient('Bread',500.0,'г')
+    recipe.add_ingredient(ingredient2)
+    assert len(recipe.ingredients) == 1
+    for el in recipe.ingredients:
+        assert el.quantity == 800.0
+def test_add_ingredient2():
+    ingredient = Ingredient('Bread',300.0,'г')
+    recipe = Recipe('Sandwich',[ingredient])
+    ingredient3 = Ingredient('Cucumber',300.0,'г')
+    recipe.add_ingredient(ingredient3)
+    assert len(recipe.ingredients) == 2
+
+def test_scale1():
+    ingredient = Ingredient('Bread',300.0,'г')
+    recipe = Recipe('Sandwich',[ingredient])
+    new_recipe = recipe.scale(2)
+    assert recipe.ingredients[0].quantity == 300.0
+    assert new_recipe.ingredients[0].quantity == 600.0
+    with pytest.raises(ValueError):
+        recipe.scale(-5)
+def test_len():
+    ingredient = Ingredient('Bread',300.0,'г')
+    recipe = Recipe('Sandwich',[ingredient])
+    assert len(recipe.ingredients) == 1
